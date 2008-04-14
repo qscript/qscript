15eb601f2bc7f99f46d5230cb22be483aa007b22
Neil Xavier DSouza <neilxdsouza@users.sourceforge.net>
2008-04-14 23:45:26 +0000

    Adding in the configure scripts and Makefile.am and friends. Note this will not compile without the symbolic links that have to be setup - refer to my note in the previous revision

---
+++ xtcc/qscript/qscript.h
@@ -0,0 +1,125 @@
+/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     IF = 258,
+     ELSE = 259,
+     FNUMBER = 260,
+     INUMBER = 261,
+     NAME = 262,
+     QTEXT = 263,
+     SP = 264,
+     MP = 265,
+     TEXT = 266,
+     VOID_T = 267,
+     INT8_T = 268,
+     INT16_T = 269,
+     INT32_T = 270,
+     FLOAT_T = 271,
+     DOUBLE_T = 272,
+     LOGOR = 273,
+     LOGAND = 274,
+     NOEQ = 275,
+     ISEQ = 276,
+     GE = 277,
+     LE = 278,
+     NOT = 279,
+     UMINUS = 280,
+     COUNT = 281,
+     IN = 282,
+     FUNC_CALL = 283,
+     CONTINUE = 284,
+     BREAK = 285
+   };
+#endif
+/* Tokens.  */
+#define IF 258
+#define ELSE 259
+#define FNUMBER 260
+#define INUMBER 261
+#define NAME 262
+#define QTEXT 263
+#define SP 264
+#define MP 265
+#define TEXT 266
+#define VOID_T 267
+#define INT8_T 268
+#define INT16_T 269
+#define INT32_T 270
+#define FLOAT_T 271
+#define DOUBLE_T 272
+#define LOGOR 273
+#define LOGAND 274
+#define NOEQ 275
+#define ISEQ 276
+#define GE 277
+#define LE 278
+#define NOT 279
+#define UMINUS 280
+#define COUNT 281
+#define IN 282
+#define FUNC_CALL 283
+#define CONTINUE 284
+#define BREAK 285
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+#line 37 "qscript.y"
+{
+	double dval;
+	int ival ;
+	char name[MY_STR_MAX];
+	char text_buf[MY_STR_MAX];
+	datatype dt;
+	struct expr * expr;
+}
+/* Line 1489 of yacc.c.  */
+#line 118 "qscript.h"
+	YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE yylval;
+
---
+++ xtcc/qscript/stmt.C
@@ -0,0 +1,54 @@
+#include "symtab.h"
+#include "stmt.h"
+#include "tree.h"
+extern vector<mem_addr_tab> mem_addr;
+extern vector <func_info*> func_info_table;
+extern ofstream debug_log_file;
+#include <fstream>
+#include <iostream>
+
+extern char * work_dir;
+stmt::~stmt(){ 
+	if (next /*
+		    && !((type==FUNC_DEFN)||(type==FUNC_TYPE))*/ ) {
+		delete next; next=0;
+	} 
+	debug_log_file << "stmt::~stmt() base destructor" << std::endl;
+}
+decl_stmt::~decl_stmt(){ 
+	debug_log_file << "deleting decl_stmt" << endl;
+	//if(symp) { delete symp; symp=0; }
+}
+
+func_info::func_info(string name, struct var_list* elist, datatype myreturn_type): 
+	fname(name), param_list(elist), return_type(myreturn_type), func_body(0), func_scope(0){
+	func_scope=new scope();
+	struct var_list* decl_list=elist;
+	while(decl_list){
+		//cout << " constructing func_info decl list names are: " << decl_list->var_name << endl;
+		struct symtab_ent* se=new struct symtab_ent;
+		se->name = strdup(decl_list->var_name.c_str());
+		se->type=decl_list->var_type;
+		func_scope->sym_tab[decl_list->var_name] = se;
+		decl_list=decl_list->next;
+	}
+}
+
+void func_info::print(FILE * fptr){
+	if(return_type >=VOID_TYPE && return_type <=DOUBLE_TYPE){
+		fprintf(fptr, "%s ", noun_list[return_type].sym );
+	} else {
+		fprintf(fptr, "Unexpected return type for function\n");
+	}
+	fprintf(fptr, "%s(", fname.c_str());
+	if (param_list) param_list->print(fptr);
+	fprintf(fptr, ");\n" );
+}
+
+func_info::~func_info(){
+	if(param_list) { delete param_list; param_list=0; }
+	//if(func_body) { delete func_body; func_body=0; }
+	// func_scope was created by in the constructor - so we delete it
+	if(func_scope) { delete func_scope; func_scope=0; }
+}
+
