b539167faa41cace0ab951386e82ff0a9372c550
Neil Xavier DSouza <neilxdsouza@users.sourceforge.net>
2008-04-28 18:08:46 +0000

    Modified mk_data.C to print a human readable data file for verification of generated tables. corrected ranges in inp12

--- xtcc/xtcc/trunk/stubs/mk_data.C
+++ xtcc/xtcc/trunk/stubs/mk_data.C
@@ -7,10 +7,17 @@ using namespace std;
 
 void dump_data(char data[], int rec_no);
 
+char* human_readable_csv_data_name = "human_readable_data.csv";
+FILE * human_readable_csv_data=fopen(human_readable_csv_data_name, "wb");
+void print_data_headers();
 
 int main(){
 	char * fname="sample2.dat";
 	int fd=open("sample2.dat", O_WRONLY|O_CREAT|O_TRUNC);
+	if(!human_readable_csv_data){
+		fprintf(stderr, "unable to open %s for writing", human_readable_csv_data_name);
+		exit(1);
+	}
 	if(fd==-1){
 		cerr << "Error trying to create file sample2.dat for writing\n";
 		exit(1);
@@ -18,9 +25,11 @@ int main(){
 	char buffer[120];
 	cout << "sizeof(buffer): " << sizeof(buffer) << endl;
 
+	print_data_headers();
 	for(int i=101;i<=400;++i){
 		
 		memset(buffer,0, sizeof(buffer));
+		fprintf(human_readable_csv_data, "%d,", i);
 		dump_data(buffer,i);
 		int n_write=write (fd, buffer, sizeof(buffer));
 		if(n_write!=sizeof(buffer)){
@@ -34,13 +43,29 @@ int main(){
 
 #include <cstring>
 #include <cstdio>
+void print_data_headers(){
+	fprintf(human_readable_csv_data, "SNO,");
+	for(char c='A';c<='Z';++c){
+		fprintf(human_readable_csv_data, "Letter_%c,", c);
+	}
+	for(char c='0';c<='9';++c){
+		fprintf(human_readable_csv_data, "Digit_%c,", c);
+	}
+	for(int i=1; i<11; ++i){
+		fprintf(human_readable_csv_data, "Integer,");
+	}
+	for(int i=1; i<11; ++i){
+		fprintf(human_readable_csv_data, "Float,");
+	}
+	fprintf(human_readable_csv_data, "\n");
+}
 void dump_data(char data[], int rec_no){
 
-	static int first_time=0;
+	static int first_time=1;
 
 	FILE * fptr=0;
-	if (!first_time) fptr= fopen("sample2.map", "wb");
-	if(!first_time){
+	if (first_time) fptr= fopen("sample2.map", "wb");
+	if(first_time){
 		fprintf(fptr, "SNO,1,4\n");
 	}
 	char * my_ptr=data;
@@ -51,20 +76,22 @@ void dump_data(char data[], int rec_no){
 	for(int i=0; i<sizeof(int); ++i){
 		*my_ptr++=*c_ptr++;
 	}
-	if(!first_time) fprintf(fptr, "char a-z,%d,%d\n", 5, 5+26);
+	if(first_time) fprintf(fptr, "char a-z,%d,%d\n", 5, 5+26-1);
 	//cout << "my_ptr-data=" << my_ptr-data << endl;
 	// output 26 letters
 	for(char c='A';c<='Z';++c){
 		*my_ptr++=c;
+		fprintf(human_readable_csv_data, "%c,", c);
 	}
 
-	if(!first_time) fprintf(fptr, "char digits 0-9,%d,%d\n", 5+26+1, 5+26+10);
+	if(first_time) fprintf(fptr, "char digits 0-9,%d,%d\n", 5+26-1+1, 5+26-1+10);
 	//output 10 digits
 	for(char c='0';c<='9';++c){
 		*my_ptr++=c;
+		fprintf(human_readable_csv_data, "%c,", c);
 	}
 	//output 10 ints: 40 bytes
-	if(!first_time) fprintf(fptr, "4 byte ints ,%d,%d\n", 5+26+10+1, 5+26+10+1*10*4);
+	if(first_time) fprintf(fptr, "4 byte ints ,%d,%d\n", 5+26-1+10+1, 5+26-1+10+1*10*4);
 	for(int i=1; i<11; ++i){
 		int no=i*100+rec_no;
 		ptr=&no;
@@ -72,8 +99,9 @@ void dump_data(char data[], int rec_no){
 		for(int j=0; j<sizeof(int); ++j){
 			*my_ptr++=*c_ptr++;
 		}
+		fprintf(human_readable_csv_data, "%d,", no);
 	}
-	if(!first_time) fprintf(fptr, "4 byte floats ,%d,%d\n",  5+26+10+1*10*4+1,5+26+10+10*4+10*4);
+	if(first_time) fprintf(fptr, "4 byte floats ,%d,%d\n",  5+26-1+10+1*10*4+1,5+26-1+10+10*4+10*4);
 	for(int i=1; i<11; ++i){
 		float no=rec_no+(static_cast<float>(rec_no)/i);
 		ptr=&no;
@@ -81,9 +109,11 @@ void dump_data(char data[], int rec_no){
 		for(int j=0; j<sizeof(float); ++j){
 			*my_ptr++=*c_ptr++;
 		}
+		fprintf(human_readable_csv_data, "%f,", no);
 	}
 
 	//cout << "my_ptr-data=" << my_ptr-data << endl;
-	if(!first_time) fclose(fptr);
-	++first_time;
+	if(first_time) fclose(fptr);
+	fprintf(human_readable_csv_data, "\n");
+	if(first_time) first_time=0;
 }
