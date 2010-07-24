007b403f9fb7179a72075447a50873e4f76636d7
Neil Xavier DSouza <neilxdsouza@users.sourceforge.net>
2010-07-24 10:21:29 +0000

    Added simple debugging support

---
+++ xtcc/qscript/stubs/simple_compiler/src/qscript_debug.cpp
@@ -0,0 +1,8 @@
+#include "qscript_debug.h"
+
+namespace qscript_debug {
+	const int DEBUG_UnaryExpression = 0;
+	const int DEBUG_Unary2Expression = 0;
+	const int DEBUG_BinaryExpression = 0;
+	const int DEBUG_Binary2Expression = 1;
+}
---
+++ xtcc/qscript/stubs/simple_compiler/src/qscript_debug.h
@@ -0,0 +1,12 @@
+#ifndef qscript_debug_h
+#define qscript_debug_h
+
+namespace qscript_debug {
+	extern const int DEBUG_UnaryExpression;
+	extern const int DEBUG_Unary2Expression;
+	extern const int DEBUG_BinaryExpression;
+	extern const int DEBUG_Binary2Expression;
+}
+
+
+#endif /* qscript_debug_h */
