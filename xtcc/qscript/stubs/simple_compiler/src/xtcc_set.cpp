126ef27a91beda9ab11cbb4ad49f24f4d55e180f
Neil Xavier DSouza <neilxdsouza@users.sourceforge.net>
2010-08-14 06:16:28 +0000

    Added strong checks for ranges and individual elements

--- xtcc/qscript/stubs/simple_compiler/src/xtcc_set.cpp
+++ xtcc/qscript/stubs/simple_compiler/src/xtcc_set.cpp
@@ -4,6 +4,8 @@
  *  Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009 Neil Xavier D'Souza
  */
 #include "xtcc_set.h"
+#include "qscript_parser.h"
+#include <sstream>
 
 XtccSet::XtccSet(): range(0){ 
 }
@@ -26,11 +28,86 @@ void XtccSet::reset(){
 	indiv.clear();
 }
 
-void XtccSet::add_range(int n1, int n2){
-	range.push_back( pair<int,int>(n1, n2));
+void XtccSet::add_range(int n1, int n2)
+{
+	using std::endl;
+	using std::stringstream;
+
+	for(	set<int>::iterator it=indiv.begin(); it!=indiv.end(); ++it){
+		int v = *it;
+		if(v>=n1 && v <= n2){
+			stringstream err_msg;
+			err_msg << "range given: " << n1 << " - " << n2
+				<< " makes the individual entry: "  << v << " redundant";
+			print_err(compiler_sem_err, err_msg.str().c_str()
+					, qscript_parser::line_no
+					, __LINE__, __FILE__ );
+		}
+	}
+
+	for(int i=0; i<range.size(); ++i){
+		if(n1 <= range[i].first && n2 >= range[i].second){
+			stringstream err_msg;
+			err_msg << "range given: " << n1 << " - " << n2
+				<< " is completely contains in an earlier range:" 
+				<< range[i].first << " - " << range[i].second << endl;
+			print_err(compiler_sem_err, err_msg.str().c_str()
+					, qscript_parser::line_no
+					, __LINE__, __FILE__ );
+		} else if ( n1 >= range[i].first && n2 <= range[i].second){ 
+			stringstream err_msg;
+			err_msg << "range given: " << n1 << " - " << n2
+				<< " is completely contained in an earlier range:" 
+				<< range[i].first << " - " << range[i].second << endl;
+			print_err(compiler_sem_err, err_msg.str().c_str()
+					, qscript_parser::line_no
+					, __LINE__, __FILE__ );
+		} else if (n1 <= range[i].first && 
+				n2 >= range[i].first && n2<=range[i].second) {
+			stringstream err_msg;
+			err_msg << "range given: " << n1 << " - " << n2
+				<< " is partially contained in an earlier range:" 
+				<< range[i].first << " - " << range[i].second << endl;
+			print_err(compiler_sem_err, err_msg.str().c_str()
+					, qscript_parser::line_no
+					, __LINE__, __FILE__ );
+		} else if (n2 > range[i].second && 
+				n1 >= range[i].first && n1<=range[i].second) {
+			stringstream err_msg;
+			err_msg << "range given: " << n1 << " - " << n2
+				<< " is partially contained in an earlier range:" 
+				<< range[i].first << " - " << range[i].second << endl;
+			print_err(compiler_sem_err, err_msg.str().c_str()
+					, qscript_parser::line_no
+					, __LINE__, __FILE__ );
+		} else if ( 	(n1 < range[i].first && n2 < range[i].first)
+			    ||	(n1 > range[i].second && n2 > range[i].second)){
+			// this is the allowed case
+			range.push_back( pair<int,int>(n1, n2));
+		} else {
+			stringstream err_msg;
+			err_msg << "internal compiler programming error contact author";
+			print_err(compiler_internal_error, err_msg.str().c_str()
+					, qscript_parser::line_no
+					, __LINE__, __FILE__ );
+		}
+	}
 }
 
-void XtccSet::add_indiv(int n1){
+void XtccSet::add_indiv(int n1)
+{
+	using std::endl;
+	using std::stringstream;
+	for(int i=0; i<range.size(); ++i){
+		if(n1 >= range[i].first && n1 <= range[i].second){
+			stringstream err_msg;
+			err_msg << "individual element : " << n1 << " is already contained in range: " 
+				<< range[i].first << " - " << range[i].second;
+			print_err(compiler_sem_err, err_msg.str().c_str()
+					, qscript_parser::line_no
+					, __LINE__, __FILE__ );
+		}
+	}
 	indiv.insert(n1);
 }
 
