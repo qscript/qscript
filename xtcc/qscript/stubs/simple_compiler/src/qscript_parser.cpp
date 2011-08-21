2cf56715b3fa48225f608e89dd8b2255f558c790
Neil Xavier D'Souza <nxd_in@yahoo.com>
2011-08-21 11:29:52 +0530

    merged changes in 28e6ef2f604f368868faaeca1f957d719278645f
    Added init() call to PrintNCursesMain

--- xtcc/qscript/stubs/simple_compiler/src/qscript_parser.cpp
+++ xtcc/qscript/stubs/simple_compiler/src/qscript_parser.cpp
@@ -1456,6 +1456,7 @@ void PrintNCursesMain (FILE * script, bool ncurses_flag)
 	fprintf(script, "\tprocess_options(argc, argv);\n");
 	//fprintf(script, "\tDIR * directory_ptr = 0;\n");
 	fprintf(script, "\tif (write_data_file_flag||write_qtm_data_file_flag) {\n");
+	fprintf(script, "\t	qtm_data_file_ns::init();\n");
 	fprintf(script, "\t	qtm_data_file_ns::init_exceptions();\n");
 	fprintf(script, "\t	directory_ptr = opendir(\".\");\n");
 	fprintf(script, "\t	if (! directory_ptr) {\n");
