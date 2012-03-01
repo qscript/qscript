376cddddab2a1d673b27408ab92057e37e5d61eb
Neil Xavier D'Souza <nxd_in@yahoo.com>
2012-03-01 13:54:43 +0000

    added small test support for i18n - using gettext

--- xtcc/qscript/stubs/simple_compiler/src/named_range.cpp
+++ xtcc/qscript/stubs/simple_compiler/src/named_range.cpp
@@ -29,8 +29,8 @@ void named_range::GenerateCode(StatementCompiledCode & code)
 	for(int32_t i = 0; i<stubs.size(); ++i){
 		code.quest_defns_init_code
 			<< qscript_parser::temp_name_generator.GetCurrentName()
-			<< ".push_back( stub_pair(\"" << stubs[i].stub_text
-			<< "\", " << stubs[i].code << "));"
+			<< ".push_back( stub_pair(gettext(\"" << stubs[i].stub_text
+			<< "\"), " << stubs[i].code << "));"
 			<< endl;
 	}
 
