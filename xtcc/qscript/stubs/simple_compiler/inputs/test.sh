71caf1049b65f33f200cd346dc60528d2ec1d634
Neil Xavier D'Souza <nxd_in@yahoo.com>
2012-03-07 21:32:05 +0530

    command line options to manually compile ncurses exe

---
+++ xtcc/qscript/stubs/simple_compiler/inputs/test.sh
@@ -0,0 +1,10 @@
+
+g++ -g -o vegetable.exe -Lc:\users\nxd\Progs\xtcc\xtcc\qscript\stubs\simple_compiler/lib -Ic:\users\nxd\Progs\xtcc\xtcc\qscript\stubs\simple_compiler/include -I/usr/include -mms-bitfields -If:/libraries/include/gtk-2.0 -If:/libraries/lib/gtk-2.0/include -If:/libraries/include/atk-1.0 -If:/libraries/include/cairo -If:/libraries/include/gdk-pixbuf-2.0 -If:/libraries/include/pango-1.0 -If:/libraries/include/glib-2.0 -If:/libraries/lib/glib-2.0/include -If:/libraries/include -If:/libraries/include/freetype2 -If:/libraries/include/libpng14  
+
+-L/usr/lib vegetable.C -Lf:/libraries/lib -lgtk-win32-2.0 -lgdk-win32-2.0 -latk-1.0 -lgio-2.0 -lpangowin32-1.0 -lgdi32 -lpangocairo-1.0 -lgdk_pixbuf-2.0 -lpango-1.0 -lcairo -lgobject-2.0 -lgmodule-2.0 -lgthread-2.0 -lglib-2.0 -lintl   -lqscript_runtime -lqscript_gtk_runtime  -lpdcurses
+
+g++ -static -g -o vegetable.exe -L../lib -I../include -I/c/usr/include -L/c/usr/lib vegetable.C -lqscript_runtime  -lpdcurses -lintl
+
+g++ -g -o vegetable.exe -L../lib -I../include -Ic:/usr/include -mms-bitfields -If:/libraries/include/gtk-2.0 -If:/libraries/lib/gtk-2.0/include -If:/libraries/include/atk-1.0 -If:/libraries/include/cairo -If:/libraries/include/gdk-pixbuf-2.0 -If:/libraries/include/pango-1.0 -If:/libraries/include/glib-2.0 -If:/libraries/lib/glib-2.0/include -If:/libraries/include -If:/libraries/include/freetype2 -If:/libraries/include/libpng14  -Lc:/usr/lib -Lf:/libraries/lib vegetable.C -lgtk-win32-2.0 -lgdk-win32-2.0 -latk-1.0 -lgio-2.0 -lpangowin32-1.0 -lgdi32 -lpangocairo-1.0 -lgdk_pixbuf-2.0 -lpango-1.0 -lcairo -lgobject-2.0 -lgmodule-2.0 -lgthread-2.0 -lglib-2.0 -lintl   -lqscript_runtime -lqscript_gtk_runtime  -lpdcurses
+
+
