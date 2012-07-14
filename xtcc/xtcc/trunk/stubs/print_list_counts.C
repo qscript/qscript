aa7844fb4e21a495e23abf1d6a90d467de1e1360
Neil Xavier D'Souza <nxd_in@yahoo.com>
2012-07-14 21:26:40 +0530

    forgot to add this into the previous commit

--- xtcc/xtcc/trunk/stubs/print_list_counts.C
+++ xtcc/xtcc/trunk/stubs/print_list_counts.C
@@ -1,5 +1,6 @@
+#include <map>
+#include <string>
 template <class T>
-void print_list_summ(map<T,int> &m);
+void print_list_summ(std::map<T,int> &m, std::string var_name, std::string text);
 void print_list_counts(){
-print_list_summ(list0 );
 }
