db50cfc3d6d3ba94071554c6c744f76af40fa947
Neil Xavier D'Souza <nxd_in@yahoo.com>
2012-08-26 01:20:59 +0530

    beginnings of new website using twitter bootstrap

---
+++ xtcc/sourceforge-website/new-website/app.js
@@ -0,0 +1,12 @@
+jQuery(function() {
+	//alert("jQuery alive");
+$('.bs-docs-sidenav').affix({
+      offset: {
+        //top: function () { return $window.width() <= 980 ? 290 : 210 }
+	top: 0
+      , bottom: 270
+      }
+});
+
+
+});
