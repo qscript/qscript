fa995577b155720bce4c1f8786a664964654fca6
Neil Xavier D'Souza <nxd_in@yahoo.com>
2013-04-16 14:54:28 +0530

    capture serial no and display it in status bar : done

---
+++ xtcc/qscript/stubs/simple_compiler/stubs/qscript_wx2_screens.C
@@ -0,0 +1,149 @@
+// Neil Xavier D'Souza
+// 2013
+// GPL V2
+//
+// This file is for my GUI screens, without the other distractions.
+//
+// Objectives:
+//    1. Function to create screen to capture serial no
+//    2. Function to clear screen after capturing serial no
+//    3. Function to display End of Qnre screen
+//    4. Mock up function to Display Question screen : because they are already implemented in the runtime
+//    5. Mock up function to Clear Question screen   :
+#include <wx/wx.h>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::string;
+
+class ScreenApp: public wxApp
+{
+public:
+    bool OnInit();
+};
+
+class ScreenUI : public wxFrame
+{
+public:
+	ScreenUI(wxFrame *frame, const wxChar *title);
+	wxPanel *panel;
+	wxBoxSizer *panel_sizer;
+	wxStatusBar * statusBar_;
+	// related to serial no:
+	wxTextCtrl * txt_ctrl_ser_no;
+	wxBoxSizer *serial_row_sizer;
+
+	void ShowSerialNoScreen();
+	void ClearSerialNoScreen();
+	void ShowEndOfQnreScreen();
+	void ClearEndOfQnreScreen();
+
+	void get_serial_no (wxCommandEvent& event);
+private:
+    DECLARE_EVENT_TABLE()
+};
+
+
+enum MyWidgetID {
+	ID_BUTTON_SERIAL_NO = wxID_HIGHEST,
+	ID_STUBS_ROW,
+	ID_NEXT_BUTTON,
+	SingleAnswerRadioBox,
+	MultiAnswerCheckListBox
+};
+
+BEGIN_EVENT_TABLE(ScreenUI, wxFrame)
+    EVT_BUTTON(ID_BUTTON_SERIAL_NO,  ScreenUI::get_serial_no)
+    //EVT_BUTTON(ID_NEXT_BUTTON,  wxQuestionnaireGUI::handleDataInput)
+    //EVT_RADIOBOX(SingleAnswerRadioBox, wxQuestionnaireGUI::OnRadioBox)
+    //EVT_CHECKLISTBOX(MultiAnswerCheckListBox, wxQuestionnaireGUI::OnCheckboxToggle)
+END_EVENT_TABLE()
+
+
+ScreenUI::ScreenUI(wxFrame *frame,
+				const wxChar *title)
+        : wxFrame(frame, wxID_ANY, title)
+{
+	const int widths[] = { -1, 200 };
+	statusBar_ = CreateStatusBar(2);
+	SetStatusWidths(2, widths);
+	panel = new wxPanel(this, -1);
+	panel_sizer = new wxBoxSizer(wxHORIZONTAL);
+	panel->SetSizer(panel_sizer);
+	ShowSerialNoScreen();
+	Show(true);
+}
+
+// init our app: create windows
+bool ScreenApp::OnInit(void)
+{
+	ScreenUI *pFrame = new ScreenUI
+				    (
+				     NULL,
+				     _T("wxWidgets Screens for QScript")
+				    );
+	SetTopWindow(pFrame);
+
+	return true;
+}
+
+void ScreenUI::ShowSerialNoScreen()
+{
+	wxStaticText *enter_serial_no_label = new wxStaticText(panel, -1, wxT("Enter the Serial No: "));
+	txt_ctrl_ser_no = new wxTextCtrl(panel, -1);
+	wxBoxSizer *serial_row_sizer = new wxBoxSizer(wxHORIZONTAL);
+	serial_row_sizer->Add (enter_serial_no_label);
+	serial_row_sizer->Add (txt_ctrl_ser_no);
+	wxButton *button = new wxButton(panel, ID_BUTTON_SERIAL_NO, wxT("Start") /* , wxPoint(20, 20) */);
+	serial_row_sizer->Add (button);
+	panel_sizer->Add (serial_row_sizer);
+	panel_sizer->Layout();
+}
+
+
+void ScreenUI::get_serial_no(wxCommandEvent& WXUNUSED(event))
+{
+	int l_ser_no = -1;
+	cout << __PRETTY_FUNCTION__ << " was invoked" << endl;
+	//const char * entry_text =(txt_ctrl_ser_no->GetValue()).utf8_str()  ;
+	wxString wx_ser_no = (txt_ctrl_ser_no->GetValue());
+	string narrow_text (wx_ser_no.utf8_str()  );
+	cout << narrow_text << endl;
+	//txt_ctrl_ser_no->GetValue();
+	//wxTextEntryDialog * sr_no = new wxTextEntryDialog(this, wxT("Please enter a serial no"), wxT("Serial No"));
+	//if (sr_no->ShowModal() == wxID_OK) {
+	//	cout << (sr_no->GetValue()).utf8_str() << endl;
+	//} else {
+	//}
+
+	if (narrow_text.length() == 0 || narrow_text.length() > 7)
+	{
+		//le_data_->setText("You have entered a very long serial number");
+		cout << "You have entered a very long serial number" << endl;
+	}
+	else
+	{
+		l_ser_no = strtol (narrow_text.c_str(), 0, 10);
+		if (l_ser_no > 0)
+		{
+			cerr << __PRETTY_FUNCTION__ << " hide serialNoEntry_ later" << endl;
+			//gtk_widget_hide (serialNoEntry_);
+			//qapp->return_ser_no(l_ser_no, qapp->theQuestionnaire_);
+			//theQuestionnaire_ = make_questionnaire();
+			//callback_get_ser_no_from_ui (l_ser_no, theQuestionnaire_);
+			wxString serial_status_info = wxString(wxT("Serial No: ")) + wx_ser_no;
+			statusBar_->SetStatusText (serial_status_info, 1);
+		} else {
+			cout << "strtol failed: l_ser_no: " << l_ser_no << endl;
+		}
+	}
+
+}
+
+
+IMPLEMENT_APP(ScreenApp)
+
