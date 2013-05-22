da2414a694c49317ba61a20e4fed594ebca8937c
Neil Xavier D'Souza <nxd_in@yahoo.com>
2013-05-22 13:32:19 +0530

    need to commit to switch branch - should figure out git stash

--- xtcc/qscript/stubs/compiler-ui/qscript_compiler_ui.cpp
+++ xtcc/qscript/stubs/compiler-ui/qscript_compiler_ui.cpp
@@ -5,6 +5,8 @@
 #include "wx/filepicker.h"
 
 #include <iostream>
+#include <sstream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -22,6 +24,13 @@ public:
 	void OnFileChange(wxFileDirPickerEvent& event);
 	void OnCheckBox(wxCommandEvent &event);
 	void CreateCheckListbox(long flags = 0);
+	void OnCheckboxToggle(wxCommandEvent& event);
+	void RunCompiler(wxCommandEvent& event);
+	//set<int32_t> cbData_;
+	wxStatusBar * statusBar_;
+	string compile_file_and_path_;
+	string cwd_;
+
 private:
 	DECLARE_EVENT_TABLE()
 };
@@ -41,6 +50,8 @@ enum {
 BEGIN_EVENT_TABLE(QScriptCompilerUI, wxFrame)
 	EVT_FILEPICKER_CHANGED(FPControlID, QScriptCompilerUI::OnFileChange)
 	EVT_CHECKBOX(wxID_ANY, QScriptCompilerUI::OnCheckBox)
+	EVT_CHECKLISTBOX(CBControlID, QScriptCompilerUI::OnCheckboxToggle)
+	EVT_BUTTON(BtnRunCmdID, QScriptCompilerUI::RunCompiler)
 END_EVENT_TABLE()
 
 /*
@@ -55,6 +66,11 @@ QScriptCompilerUI::QScriptCompilerUI(const wxString& title)
 		       //, wxDefaultSize
 		       )
 {
+	const int widths[] = { -1, 200 };
+	statusBar_ = CreateStatusBar(2);
+	SetStatusWidths(2, widths);
+
+
 	panel_ = new wxPanel(this, -1);
 
 	//wxBoxSizer *vtUIContainer_ = new wxBoxSizer(wxHORIZONTAL);
@@ -126,6 +142,8 @@ void QScriptCompilerUI::OnFileChange(wxFileDirPickerEvent& event)
 		<< "' ! The current working directory is '"
 		<< wxGetCwd().utf8_str()
 		<< endl;
+	compile_file_and_path_ = event.GetPath().utf8_str();
+	cwd_ = wxGetCwd().utf8_str();
 }
 
 void QScriptCompilerUI::OnCheckBox(wxCommandEvent &event)
@@ -193,3 +211,83 @@ void QScriptCompilerUI::CreateCheckListbox(long flags)
 	//m_pListBox->Select(3);
 }
 
+void QScriptCompilerUI::OnCheckboxToggle(wxCommandEvent& event)
+{
+	unsigned int nItem = event.GetInt();
+
+	wxLogStatus(this, wxT("item %d was %schecked"), nItem,
+		      compilerOptionsCBox_->IsChecked(nItem) ? wxT("") : wxT("un"));
+	// int code = rbQnreCodeMap_[nItem];
+	// if (compilerOptionsCBox_->IsChecked(nItem)) {
+	// 	cbData_.insert (code);
+	// } else {
+	// 	cbData_.erase (code);
+	// }
+	// cout << "selected values are: " << endl;
+	// for (set<int32_t>::iterator it = cbData_.begin(); it != cbData_.end(); ++it) {
+	// 	cout << " " << *it;
+	// }
+	cout << endl;
+
+}
+
+void QScriptCompilerUI::RunCompiler(wxCommandEvent& event)
+{
+	cout << __PRETTY_FUNCTION__ << endl;
+
+	for (int i=0; i<4; ++i) {
+		if (compilerOptionsCBox_->IsChecked(i)) {
+			cout << "item " << i << "is Checked" << endl;
+		} else {
+			cout << "item " << i << "is UnChecked" << endl;
+		}
+	}
+	enum CompilerFlags {
+		OptimizeFlag,
+		NiceMapFlag,
+		NCursesUIFlag,
+		StaticCompileFlag
+	};
+
+	stringstream compiler_options;
+	if (compilerOptionsCBox_->IsChecked(OptimizeFlag)) {
+		compiler_options << " -o ";
+	}
+	if (compilerOptionsCBox_->IsChecked(NiceMapFlag)) {
+		compiler_options << " -m ";
+	}
+	if (compilerOptionsCBox_->IsChecked(NCursesUIFlag)) {
+		compiler_options << " -n ";
+	}
+	if (compilerOptionsCBox_->IsChecked(StaticCompileFlag)) {
+		compiler_options << " -s ";
+	}
+	cout << "compiler_options: " << compiler_options.str() << endl;
+	if (compile_file_and_path_.length() == 0) {
+		cout << "please choose a file using the FilePicker" << endl;
+		return;
+	}
+	{
+		string home = getenv("QSCRIPT_HOME");
+		cout << "QSCRIPT_HOME: is " << home << endl;
+	}
+	string qscript_home =  cwd_ + "/../simple_compiler";
+	string qscript_home_env = "QSCRIPT_HOME=" + qscript_home;
+	char * s (strdup(qscript_home_env.c_str()));
+	putenv (s);
+	{
+		string home = getenv("QSCRIPT_HOME");
+		cout << "QSCRIPT_HOME: now is " << home << endl;
+	}
+	// memory leak
+	stringstream cmd;
+	size_t pos = compile_file_and_path_.find_last_of('/');
+	string proj_dir = compile_file_and_path_(0, pos);
+	cout << "proj_dir: " << proj_dir << endl;
+	cmd << qscript_home << "/bin/qscript " << compiler_options.str()
+		<< " -f " << compile_file_and_path_
+		;
+	cout << "cmd: " << cmd.str() << endl;
+	//int rval = system;
+
+}
