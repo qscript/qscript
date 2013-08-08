55041400294a9fa4d350610854ff7d6301884188
Neil Xavier D'Souza <nxd_in@yahoo.com>
2013-08-08 14:46:54 +0530

    sample android phonegap app
    - replace index.html  in assets/www with the html file from the compiler
      output. For example
    
    simple_compiler> cd inputs
    inputs> ../bin/qscript_wq2    -o -m --emscripten -f vegetable-old.qscript
    
     cp vegetable-old.html ../../qscript_android_phonegap_app/assets/www/index.html
    
    Currently the committed shell file shell-phonegap...html
    is trying to open the data file for the serial no entered in a hardcoded
    manner. I am trying to fix this behaviour but unable to solve the bug,
    some issue in the filesystem api maybe? as the fail function does not
    execute when it is supposed to. Need to figure out a way to solve this
    problem.

---
+++ xtcc/qscript/stubs/qscript_android_phonegap_app/gen/qscript/qscript_android_phonegap_app/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package qscript.qscript_android_phonegap_app;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
